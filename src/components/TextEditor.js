9a7aa9fe2c371644d165a5ee7f83aae511bfcaf8
omniman-dev <abhinavbarawal26@gmail.com>
2025-06-11 15:14:19 +0530

    Update TextEditor.js

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+const TextEditor = ({ text, onChange }) => {
+  return (
+    <div className="editor-box">
+      <textarea
+        value={text}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Edit PDF text here..."
+      />
+    </div>
+  );
+};
+
+export default TextEditor;
